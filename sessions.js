7ad1ca048cd7659a2e8068f2f724a195e02b3f6d
Julian Peters <julianpeters@Julians-MacBook-Pro.local>
2018-12-05 21:48:09 -0500

    v1

--- sessions.js
+++ sessions.js
@@ -1,15 +1,15 @@
 
-var sessions = [
+var rgbColor = [
     {id:0 , red: 255},
     {id:1 , green:255},
     {id:2 , blue:255}
 ];
 
 exports.findAll = function (req, res, next) {
-    res.send(sessions);
+    res.send(rgbColor);
 };
 
 exports.findById = function (req, res, next) {
     var id = req.params.id;
-    res.send(sessions[id]);
+    res.send(rgbColor[id]);+
 };
