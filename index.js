ca0ed3fe04ebe530b12ea0ec90fa2c11b4826918
Julian Peters <julianpeters@Julians-MacBook-Pro.local>
2018-12-05 22:04:43 -0500

    v1

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.post('/', function(req, res)){
+    console.log('POST /');
+    console.dir(req.body);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('thanks');
+}
